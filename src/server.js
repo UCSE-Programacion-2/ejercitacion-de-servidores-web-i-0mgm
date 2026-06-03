40937748b7ee5660a16b3892c563ffc47bec5555
Mauricio Maizares <mauriciogmaizares@gmail.com>
2026-06-02 23:10:10 -0300

    Ejercitacion de servidores realizado

--- src/server.js
+++ src/server.js
@@ -1,15 +1,27 @@
-/**
- * Módulo que crea y configura el servidor HTTP
- */
 const http = require('http');
 
-/**
- * Crea un servidor HTTP que utiliza la función router para manejar las solicitudes
- * @returns {http.Server} Instancia del servidor HTTP
- */
 function createServer() {
   const server = http.createServer((req, res) => {
-    /* Aquí la lógica del enrutador */
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+
+    const ruta = req.url;
+
+    if (ruta === '/') {
+      res.writeHead(200);
+      res.end('Bienvenid@s! Gracias por tu visita.');
+    } else if (ruta === '/nosotros') {
+      res.writeHead(200);
+      res.end('Bienvenid@s a saber + de nosotros :)');
+    } else if (ruta === '/cursos') {
+      res.writeHead(200);
+      res.end('Te interesan nuestros cursos. Vení a conocer la oferta!');
+    } else if (ruta === '/contacto') {
+      res.writeHead(200);
+      res.end('Si querés contactarnos, hacelo a este Email: :)');
+    } else {
+      res.writeHead(404);
+      res.end('No se ha encontrado la ruta ingresada.');
+    }
   });
 
   return server;
